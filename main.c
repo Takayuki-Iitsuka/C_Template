12158efc32d9bfbccab9008e8b346d117876b581
takayuki.iitsuka <takayuki.iitsuka@gmail.com>
2023-06-11 15:12:44 +0900

    プロジェクト ファイルを追加します。

---
+++ main.c
@@ -0,0 +1,25 @@
+﻿// main.c
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define MY_PI 3.141592653589793238462643
+
+//int main(void)
+int main(int argc, char* argv[])
+{
+
+
+
+	printf("Hello World!\n");
+
+#ifndef _DEBUG
+	system("pause");
+	//char hoge = getchar();
+#endif
+
+	return 0;
+}
+
+// End
